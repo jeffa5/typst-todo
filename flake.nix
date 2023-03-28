d73edd0d6b35e06a23ebb63537123e456bbca89c
Andrew Jeffery <dev@jeffas.io>
2023-03-28 13:41:38 +0100

    Add nix package to build and script to watch

--- flake.nix
+++ flake.nix
@@ -10,6 +10,39 @@
       inherit system;
     };
   in {
+    packages.${system} = {
+      watch = pkgs.writeShellApplication {
+        name = "watch";
+        text = ''
+          ${pkgs.typst}/bin/typst \
+            --watch \
+            doc.typ \
+            doc.pdf
+        '';
+      };
+
+      default = pkgs.stdenvNoCC.mkDerivation {
+        name = "typst-todo";
+
+        src = pkgs.lib.cleanSource ./.;
+
+        buildPhase = ''
+          runHook preBuild
+          ${pkgs.typst}/bin/typst \
+            --root $src \
+            $src/doc.typ \
+            doc.pdf
+          runHook postBuild
+        '';
+
+        installPhase = ''
+          runHook preInstall
+          install -m644 -D *.pdf --target $out/
+          runHook postInstall
+        '';
+      };
+    };
+
     devShells.${system}.default = pkgs.mkShell {
       packages = with pkgs; [
         typst
